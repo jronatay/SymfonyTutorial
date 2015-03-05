982c297eda0ef1283a03fdfbb53334b578b04b0e
jronatay <jronatay25@gmail.com>
2015-03-05 12:29:20 +0800

    update

--- src/Acme/DemoBundle/Controller/RandomController.php
+++ src/Acme/DemoBundle/Controller/RandomController.php
@@ -8,16 +8,21 @@
 
 namespace Acme\DemoBundle\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
 /**
  * Description of RandomController
  *
  * @author Justine
  */
-class RandomController {
+class RandomController extends Controller {
+
     //put your code here
-    
-    public function indexAction($limit){
-        return new response('<html><body>Number: '.rand(1,$limit).'</body></html>');
+
+    public function indexAction($limit) {
+        $number = rand(1, $limit);
+
+        return $this->render('AcmeBundle:Random:index.html.twig', array('number' => $number));
     }
+
 }
